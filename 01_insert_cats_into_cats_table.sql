aac7602464ba35af614434532c2e31aa7a099ada
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-22 14:36:08 +0300

    select, update, delete

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,11 @@
+INSERT INTO cats (name, age, breed) VALUES ("Lil' Bub", 5, 'American Shorthair');
+INSERT INTO cats (name, age, breed) VALUES ('Hannah', 1, 'Tabby');
+
+-- SELECT name, age FROM cats;
+-- SELECT DISTINCT name FROM cats;
+-- SELECT * FROM cats WHERE name = 'Ramses';
+-- SELECT * FROM cats WHERE name LIKE '%ramses%';
+-- SELECT * FROM cats WHERE age < 4;
+
+-- UPDATE cats SET name="Hana" WHERE name = "Hannah";
+-- DELETE FROM cats WHERE id = 3;
